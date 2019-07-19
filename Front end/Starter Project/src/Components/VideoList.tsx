879d360b42049acda93395844f7fcd25ed2d4eca
res550 <37566536+res550@users.noreply.github.com>
2019-07-20 10:41:39 +1200

    render

--- Front end/Starter Project/src/Components/VideoList.tsx
+++ Front end/Starter Project/src/Components/VideoList.tsx
@@ -25,7 +25,17 @@ export default class VideoList extends React.Component<IProps,IState>{
         }).then((response:any) => {
             return response.json();
         }).then((response:any)=>{
-            
+            const output:any[] = []
+            response.forEach(video => {
+                const row = (<tr>
+                    <td onClick={/** handle like */}>{video.isFavourite === true?<Star/>:<StarBorder/>}</td>
+                    <td onClick={() => this.props.play(video.webUrl)}><img src={video.thumbnailUrl}/></td>
+                    <td onClick={() => this.props.play(video.webUrl)}>{video.videoTitle}</td>
+                    <td onClick={() => this.deleteVideo(video.videoId)}><Close/></td>                    
+                    </tr>
+                    }
+                </tr>)
+            });
         })
     }
 
