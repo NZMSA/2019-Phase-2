4ec400788067f32df100205440636073ef1ae2cb
Paul Tanchareon <paul.vtan@live.com>
2019-07-21 04:00:10 +1200

    Complete SignalR project

--- Advanced Features/SignalR/Complete Project/Frontend/src/Components/VideoList.tsx
+++ Advanced Features/SignalR/Complete Project/Frontend/src/Components/VideoList.tsx
@@ -1,22 +1,26 @@
 import Close from '@material-ui/icons/Close'
 import Star from '@material-ui/icons/Star'
 import StarBorder from '@material-ui/icons/StarBorder'
+import Icon from '@material-ui/core/Icon';
 import * as React from 'react'
 
 interface IState{
+    usersCountCurrent: any
     videoList: any
 }
 
 interface IProps{
-    mount:any
-    play:any
+    mount:any,
+    play:any,
+    hubConnection:any
 }
 
 export default class VideoList extends React.Component<IProps,IState>{
     public constructor(props:any){
         super(props);
         this.state = {
-            videoList: []
+            usersCountCurrent: 0,
+            videoList: [],
         }
         this.updateList();
     }
@@ -26,7 +30,7 @@ export default class VideoList extends React.Component<IProps,IState>{
             method:'DELETE'
         }).then(() => {
             this.updateList()
-        })
+        }).then(() => {this.props.hubConnection.invoke("DeleteVideo")});
     }
 
     public playVideo = (videoUrl:string) => {
@@ -79,14 +83,30 @@ export default class VideoList extends React.Component<IProps,IState>{
     public componentDidMount = () => {
         this.props.mount(this.updateList)
         this.updateList()
+
+        this.props.hubConnection.on("VideoDeleted", ()  => {
+            this.updateList();
+            console.log('A video has been deleted.');
+        });
+
+        this.props.hubConnection.on("ShowUserCounts", (usersCount: any)  => {
+            console.log(usersCount);
+            this.setState({usersCountCurrent:usersCount});
+        });
+
     }
 
 
 
     public render() {
+        const style = { 
+            display: 'inline-flex',
+            fontSize: 25,
+            verticalAlign: 'middle',
+        }
         return (
             <div className="video-list">
-                <h1 className="play-heading"><span className="red-heading">play</span>video</h1>
+                <h1 className="play-heading"><span className="red-heading">play</span>video <span style={style}><Icon color='primary' fontSize='large'>person</Icon><b>{this.state.usersCountCurrent}</b></span></h1>
                 <table className="table">
                     {this.state.videoList}
                 </table>
